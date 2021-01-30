726c1bc7f097a0ca62b307e36200bc45bff219a8
Deathtanium <mishouro08@gmail.com>
2021-01-31 01:18:15 +0200

    Add files via upload

--- chatclientcli.py
+++ chatclientcli.py
@@ -3,6 +3,7 @@
 import sys
 import socket
 import threading
+import hashlib
 
 HOST = '84.117.187.56'
 PORT = 9090
@@ -10,9 +11,10 @@ PORT = 9090
 class Client:
     def __init__(self, host, port):
         
-        #set nickname
-        self.nickname = input("Please enter your nickname: ")
-        
+        self.username = input("Username: ")
+        self.password = input("Password: ")
+        self.passhash = hashlib.sha256(self.password.encode('utf-8')).hexdigest()
+            
         #connect to server
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port)) #host = domain name/WAN IP of the server
@@ -30,7 +32,8 @@ class Client:
         while True:
             try:
                 message = input()
-                if len(message)>1:
+                print ("\033[A                             \033[A")
+                if len(message)>1 and len(message)<128:
                     self.sock.send(message.encode('utf-8'))
             except KeyboardInterrupt:
                 print("\nClient stopping1")
@@ -41,8 +44,20 @@ class Client:
         while True:
             try:
                 message = self.sock.recv(1024).decode('utf-8')
-                if message == 'NICK':
-                    self.sock.send(self.nickname.encode('utf-8'))
+                if message == 'LOGIN':
+                    self.sock.send(f"{self.username}#{self.passhash}".encode('utf-8'))
+                    message = self.sock.recv(1024).decode('utf-8')
+                    if message == 'OK':
+                        pass #login successful
+                    elif message == 'BADUSERNAME':
+                        print("This username doesn't exist.\nContact the owner of this server if you know him in order to register an account on this server.")
+                        break
+                    elif message == 'BADPASSWORD':
+                        print("Incorrect password.")
+                        break
+                    elif message == 'ALREADYLOGGED':
+                        print("You are already logged in from another device.")
+                        break
                 #why is is sending blank data when closed?
                 elif message=='1':
                     print("\nServer shutdown")
--- chatservercli.py
+++ chatservercli.py
@@ -4,97 +4,171 @@ import os
 import sys
 import socket
 import threading
+import hashlib
+import datetime
 
-HOST = '192.168.0.122' #84.117.187.56 for 
+HOST = '192.168.0.122'
 PORT = 9090
+
+accFileName = 'accounts.txt'
+accFilePath = ''
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 server.bind((HOST, PORT))
 
 server.listen()
 
-
-
 clients = []
-nicknames = []
+usernames = []
+passhashes = []
 
+accFile = open(f"{accFilePath}{accFileName}",'r+')
+accText = accFile.readlines()
+accFile.close()
+
+for line in accText:
+    clients.append(0)
+    lin = line.split('#')
+    usernames.append(lin[0])
+    passhashes.append(lin[1].strip())
 
 def broadcast(message):
     for client in clients:
-        client.send(message.encode('utf-8'))
+        try:
+            client.send(message.encode('utf-8'))
+        except AttributeError:
+            pass
 
 def handle(client):
     while True:
         try:
             msg = client.recv(1024).decode('utf-8')
-            message = f"<{nicknames[clients.index(client)]}> {msg}"
+            message = '[{:%d-%m-%Y %H:%M}]'.format(datetime.datetime.now()) + f"<{usernames[clients.index(client)]}> {msg}"
             if len(msg) > 1:
                 #logging
-                logFile = open('log.txt', 'a')
-                logFile.write(f"\n{message}")
+                logFile = open('msg.log', 'a')
+                logFile.write(f"{message}\n")
                 logFile.close()
                 print(message)
                 
                 #send the message to everyone
-                broadcast(f"{message}")
+                broadcast(message)
             if msg == '':
                 #this should be triggered if the client becomes unreachable
-                index = clients.index(client)
-                nickname = nicknames[index]
-                nicknames.remove(nickname)
+                username=usernames[clients.index(client)]
                 
-                clients.remove(client)
-                client.shutdown(1)
+                client.close()
+                clients[clients.index(client)] = 0
                 
                 #logging
-                logFile = open('log.txt', 'a')
-                logFile.write(f"\n{nickname} disconnected.")
+                message = '[{:%d-%m-%Y %H:%M}]'.format(datetime.datetime.now()) + f"{username} disconnected.\n"
+                logFile = open('connect.log', 'a')
+                logFile2= open('msg.log', 'a')
+                logFile2.write(message)
+                logFile.write(message)
+                logFile.close()
                 logFile.close()
                 
-                print(f"{nickname} disconnected.")
+                broadcast(message.replace('\n',''))
+                
+                print(f"{username} disconnected.")
                 break
-        except:
+        except KeyboardInterrupt:
             pass
         
 def receive():              
     while True:
         client, address = server.accept()
 
-        client.send("NICK".encode('utf-8'))  #requesting nickname from client
-        nickname = client.recv(1024).decode('utf-8')         #taking their response
+        client.send("LOGIN".encode('utf-8'))                 #requesting username from client
+        logindata = client.recv(1024).decode('utf-8').split('#')         #taking their response
+        username = logindata[0]
+        passhash = logindata[1]
         
-        nicknames.append(nickname)
-        clients.append(client)
-
-        #logging
-        msg = f"\n{nickname} connected to the server from {str(address)}"
-        logFile = open('log.txt', 'a')
-        logFile.write(msg)
-        logFile.close()
-        print(msg)
+        try:
+            usernames.index(username)
+        except ValueError:
+            client.send("BADUSERNAME".encode('utf-8'))
+            client.close()
+            continue
         
-        broadcast(f"{nickname} connected to the server")
+        if clients[usernames.index(username)] != 0: 
+            client.send("ALREADYLOGGED".encode('utf-8'))
+            client.close()
+            continue
+        
+        try:
+            passhashes.index(passhash)
+        except ValueError:
+            client.send("BADPASSWORD".encode('utf-8'))
+            client.close()
+            continue
+        
+        if usernames.index(username) == passhashes.index(passhash):
+            client.send("OK".encode('utf-8'))
+            
+            clients[usernames.index(username)] = client
+            try:
+                logFile = open('msg.log', 'r')
+                client.send(logFile.read().encode('utf-8'))
+                logFile.close()
+            except:
+                pass
+            
+            address = address[0]
+            
+            msg1 = '[{:%d-%m-%Y %H:%M}]'.format(datetime.datetime.now()) + f"{username} connected to the server\n"
+            msg2 = '[{:%d-%m-%Y %H:%M}]'.format(datetime.datetime.now()) + f"{username} connected to the server from {str(address)}\n"
+            
+            #logging
+            logFile = open('msg.log', 'a')
+            logFile2= open('connect.log','a')
+            logFile.write(msg1)
+            logFile2.write(msg2)
+            logFile.close()
+            logFile2.close()
+            print(msg2)
+            
+            broadcast(msg1.replace('\n',''))
 
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.daemon = True
-        thread.start()
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.daemon = True
+            thread.start()
+            
+        elif usernames.index(username) != passhashes.index(passhash):
+            client.send("BADPASSWORD".encode('utf-8'))
+            client.close()
+
 
 print ("Server running")
+try:
+    logFile = open('log.txt', 'r')
+    print(logFile.read())
+    logFile.close()
+except:
+    pass
+
 try:
     receive()
 except KeyboardInterrupt:
     broadcast('1')
     for client in clients:
-        client.close()
+        try:
+            client.close()
+        except AttributeError:
+            pass
     server.close()
     print("\nServer is stopping")
     exit()
-except BrokenPipeError:
+except BrokenPipeError or UnicodeDecodeError:
     for client in clients:
-        client.close()
+        try:
+            client.close()
+        except AttributeError:
+            pass
     server.close()
     clients = []
     nicknames = []
     os.execv(sys.argv[0], sys.argv)
-    
